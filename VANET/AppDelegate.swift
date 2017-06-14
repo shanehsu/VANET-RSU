2b097f4bad759bad7de93fd56aac11ed7ca1c32e
徐鵬鈞 <hsu.pengjun@icloud.com>
2017-06-15 02:37:06 +0800

    Initial Commit

---
+++ VANET/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  VANET
+//
+//  Created by 徐鵬鈞 on 2017/6/15.
+//  Copyright © 2017年 Nameless Apps. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ VANET/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  VANET
+//
+//  Created by 徐鵬鈞 on 2017/6/15.
+//  Copyright © 2017年 Nameless Apps. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
